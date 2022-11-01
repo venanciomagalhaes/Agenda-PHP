7704b57438d4160d36581944eeffbd633091bffb
D. Venancio Magalhaes <venanciomagalhaesd@gmail.com>
2022-11-01 20:48:17 -0300

    js-personalizado

--- assets/js/personalizado.js
+++ assets/js/personalizado.js
@@ -1,58 +1,30 @@
-
-$('#telefone').mask('(31) 9 9999-9999')
-$('#editar-agendamento').validate({
-    rules: {
-        'nome-cliente': {
-            required: true,
-            minlength: 3,
-        },
-
-        'data': {
-            required: true,
-            date: true
-        },
-        'telefone': {
-            required: true,
-        },
-        'horario': {
-            required: true,
-        },
-        'servico': {
-            required: true,
-            minlength: 10,
-            maxlength: 2500,
-        }
-    }
-})
-
-$('#cadastrar-agendamento').validate({
-    rules: {
-        'nome-cliente': {
-            required: true,
-            minlength: 3,
-        },
-
-        'data': {
-            required: true,
-            date: true
-        },
-        'telefone': {
-            required: true,
-        },
-        'horario': {
-            required: true,
-        },
-        'servico': {
-            required: true,
-            minlength: 10,
-            maxlength: 2500,
-        }
-    }
-})
-
-
 $(document).ready(function () {
 
     $('input[type="tel"]').mask('(31) 9 9999-9999');
+
+    $('form').validate({
+        rules: {
+            'nome-cliente': {
+                required: true,
+                minlength: 3,
+            },
+
+            'data': {
+                required: true,
+                date: true
+            },
+            'telefone': {
+                required: true,
+            },
+            'horario': {
+                required: true,
+            },
+            'servico': {
+                required: true,
+                minlength: 10,
+                maxlength: 2500,
+            }
+        }
+    })
 
 });
